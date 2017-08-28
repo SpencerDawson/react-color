14a2a2c37ba5760b68091641df618673716ee45e
Spencer Dawson <rourke.dawson@gmail.com>
2017-08-28 18:38:59 -0400

    Revert change to onSwatchHover. I misinterpreted it's purpose. Will need to review how it will impact the Palette. #305

--- src/components/common/ColorWrap.js
+++ src/components/common/ColorWrap.js
@@ -31,13 +31,27 @@ export const ColorWrap = (Picker) => {
         this.props.onChange && this.props.onChange(colors, event)
       }
     }
-
+  
+    handleSwatchHover = (data, event) => {
+      const isValidColor = color.simpleCheckForValidColor(data)
+      if (isValidColor) {
+        const colors = color.toState(data, data.h || this.state.oldHue)
+        this.setState(colors)
+        this.props.onSwatchHover && this.props.onSwatchHover(colors, event)
+      }
+    }
+  
     render() {
+        const optionalEvents = {}
+        if (this.props.onSwatchHover) {
+          optionalEvents.onSwatchHover = this.handleSwatchHover
+        }
       return (
         <Picker
           { ...this.props }
           { ...this.state }
           onChange={ this.handleChange }
+          { ...optionalEvents }
         />
       )
     }
